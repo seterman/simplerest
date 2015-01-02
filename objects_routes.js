4218555f2785bdbaa4f6dcd5ee419fd6343859b5
Stacey Terman <seterman@mit.edu>
2015-01-02 18:27:22 -0500

    Add GET api/objects route

--- objects_routes.js
+++ objects_routes.js
@@ -1,3 +1,24 @@
 var router = require('express').Router();
 
+// The database is accessible through req.db, which was pre-seeded in server.js
+
+// Get a list of uids for all objects
+router.get('/', function(req, res, next) {
+    var objs = req.db.get('objs');
+
+    objs.find({}, function(err, docs) {
+        if (err) {
+            var e = new Error('Database error');
+            next(e);
+            return;
+        }
+        console.log(req.baseUrl);
+
+        var results = docs.map(function(doc) {
+            return { url: req.baseUrl + '/' + doc._id };
+        });
+        res.send(results);
+    });
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var monk = require('monk');
 
 var objects = require('./objects_routes');
 
-// connect to database
-var connection_string = 'localhost/';
+// Connect to database
+var connection_string = 'localhost/simplerest';
 if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
     connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' +
     process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' +
@@ -17,30 +17,39 @@ if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
 var db = monk(connection_string);
 
 var app = express();
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 
-// routes to /api/objects
+// Pre-seed the req with a reference to the database.
+app.use(function(req, res, next) {
+    req.db = db;
+    next();
+});
+
+// Handle specified routes
 app.use('/api/objects', objects);
 
-// catch any unhandled routes
+// Catch unhandled routes, pass to the error handler
 app.use(function(req, res, next) {
-    console.log(req.method);
-    console.log(req.url);
+    var err = new Error('URL not found');
+    next(err);
+});
+
+// Error handler
+app.use(function(err, req, res, next) {
+    // Code for getting the full URL based on this question on StackOverflow:
+    // http://stackoverflow.com/questions/10183291/how-to-get-the-full-url-in-express-js
+    var fullURL = req.protocol + '://' + req.get('host') + req.url;
 
     res.send({
         verb: req.method,
-        url: req.url,
-        message: 'URL not found'
+        url: fullURL,
+        message: err.message || 'URL not found'
     });
 });
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-console.log("server listening on port ", port);
+console.log('Server listening on port', port);
 app.listen(port, ip);
